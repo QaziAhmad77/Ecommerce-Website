ca61f1919e4066426df109a34c426e460a082044
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-12-16 18:16:53 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,48 @@
+import { productsData } from './api/Api';
 import Header from './components/Header';
+import Product from './components/Product';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <>
+        <Header />
+        <Outlet />
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+          loader: productsData,
+        },
+        {
+          path: '/product/:id',
+          element: <Product />,
+        },
+        {
+          path: '/cart',
+          element: <Cart />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className='font-poppins'>
-      <Header />
-      <Home />
+    <div className="font-poppins">
+      <RouterProvider router={router} />
     </div>
   );
 };
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,4 +1,18 @@
 import headerLogo from '../assets/headerLogo.jpg';
 import cart from '../assets/cart.jpg';
 import userLogo from '../assets/userLogo.jpg';
-export { headerLogo, cart, userLogo };
+import bannerImgOne from '../assets/banner/bannerImgOne.jpg';
+import bannerImgTwo from '../assets/banner/bannerImgTwo.jpg';
+import bannerImgThree from '../assets/banner/bannerImgThree.jpg';
+import bannerImgFour from '../assets/banner/bannerImgFour.jpg';
+import bannerImgFive from '../assets/banner/bannerImgFive.jpg';
+export {
+  headerLogo,
+  cart,
+  userLogo,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  bannerImgFour,
+  bannerImgFive,
+};
